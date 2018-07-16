2c672717636f7e404998441b483eab323abcb788
jsvivien <manhhungacoustic@gmail.com>
2018-07-16 11:23:42 +0700

    Start Project

---
+++ app/src/main/java/com/example/tmh/tablayoutviewpager/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.tmh.tablayoutviewpager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
